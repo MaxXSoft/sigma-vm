9ebad7831fbcbb4df6d923680813caead4caab8a
MaxXSoft <x@MaxXSoft.net>
2023-08-04 21:17:56 +0800

    (consts) added more implementations for `Const`

--- src/bytecode/consts.rs
+++ src/bytecode/consts.rs
@@ -1,11 +1,94 @@
 //! Definitions about constant pool.
 
+use std::{mem, slice};
+
 /// Constant.
 #[derive(Debug)]
 pub struct Const {
   kind: ConstKind,
   data: Box<[u8]>,
 }
+
+impl Const {
+  /// Returns the address of the current constant.
+  pub fn addr(&self) -> u64 {
+    self.data.as_ptr() as u64
+  }
+
+  /// Returns the value of the current constant as an integer,
+  /// or [`None`] if value is unavailable.
+  ///
+  /// # Safety
+  ///
+  /// The kind matches the data.
+  pub unsafe fn value(&self) -> Option<u64> {
+    macro_rules! value {
+      ($($kind:ident => $ty:ty),* $(,)?) => {
+        match self.kind {
+          $(ConstKind::$kind => Some(*(self.data.as_ptr() as *const $ty) as u64),)*
+          _ => None,
+        }
+      };
+    }
+    value! {
+      I8 => i8,
+      U8 => u8,
+      I16 => i16,
+      U16 => u16,
+      I32 => i32,
+      U32 => u32,
+      I64 => i64,
+      U64 => u64,
+      F32 => u32,
+      F64 => u64,
+    }
+  }
+}
+
+/// Helper macro for implementing [`From<T>`] for [`Const`].
+macro_rules! impl_from {
+  ($ty:ident<$g:ident: $b:path>, $kind:ident) => {
+    impl<$g> From<$ty<$g>> for Const
+    where
+      $g: $b,
+    {
+      fn from(value: $ty<$g>) -> Self {
+        Self {
+          kind: ConstKind::$kind,
+          data: Box::from(unsafe {
+            slice::from_raw_parts(&value as *const _ as *const u8, mem::size_of_val(&value))
+          }),
+        }
+      }
+    }
+  };
+  ($ty:ty, $kind:ident) => {
+    impl From<$ty> for Const {
+      fn from(value: $ty) -> Self {
+        Self {
+          kind: ConstKind::$kind,
+          data: Box::from(unsafe {
+            slice::from_raw_parts(&value as *const _ as *const u8, mem::size_of_val(&value))
+          }),
+        }
+      }
+    }
+  };
+}
+
+impl_from!(i8, I8);
+impl_from!(u8, U8);
+impl_from!(i16, I16);
+impl_from!(u16, U16);
+impl_from!(i32, I32);
+impl_from!(u32, U32);
+impl_from!(i64, I64);
+impl_from!(u64, U64);
+impl_from!(f32, F32);
+impl_from!(f64, F64);
+impl_from!(Str<Bytes: Array<u8>>, Str);
+impl_from!(Object<Offsets: Array<u64>>, Obj);
+impl_from!(Raw<Bytes: Array<u8>>, Raw);
 
 /// Kind of constant.
 #[repr(u8)]
