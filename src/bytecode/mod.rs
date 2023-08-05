e2c00e96028e6054c130e8b0d8e783ff6004c628
MaxXSoft <x@MaxXSoft.net>
2023-08-05 17:17:57 +0800

    (bytecode) made all submodule public

--- src/bytecode/mod.rs
+++ src/bytecode/mod.rs
@@ -1,7 +1,7 @@
-mod consts;
-mod insts;
-mod reader;
-mod writer;
+pub mod consts;
+pub mod insts;
+pub mod reader;
+pub mod writer;
 
 /// Magic number of the bytecode file.
 const MAGIC: [u8; 4] = [b'S', b'g', b'm', b'a'];
