5caa255dde8d076ad77a910064978d3edd3efcd1
MaxXSoft <x@MaxXSoft.net>
2023-08-06 17:25:12 +0800

    (heap) added more implementations

--- src/interpreter/heap.rs
+++ src/interpreter/heap.rs
@@ -1,42 +1,168 @@
+use std::alloc::{self, Layout};
+use std::collections::{BTreeMap, HashMap};
+use std::slice;
+
 /// Managed heap interface.
 pub trait Heap {
   /// Creates a new heap.
   fn new() -> Self;
+
+  /// Allocates a new memory with the given layout.
+  /// Returns the pointer of the allocated memory.
+  fn alloc(&mut self, layout: Layout) -> u64;
+
+  /// Allocates a new memory with the given layout and object metadata.
+  /// Returns the pointer of the allocated memory.
+  fn alloc_obj(&mut self, layout: Layout, obj: Obj) -> u64;
+
+  /// Deallocates the given pointer.
+  fn dealloc(&mut self, ptr: u64);
+
+  /// Returns the size of all allocated memory in bytes.
+  fn size(&self) -> usize;
+
+  /// Returns the immutable memory address of the given pointer.
+  fn addr(&self, ptr: u64) -> *const ();
+
+  /// Returns the mutable memory address of the given pointer.
+  fn addr_mut(&mut self, ptr: u64) -> *mut ();
+
+  /// Returns a reference to the heap object metadata of the given pointer, or
+  /// [`None`] if the corresponding heap memory is not allocated for an object.
+  fn obj(&self, ptr: u64) -> Option<&Obj>;
 }
 
 /// Managed heap with memory out of bounds checking.
 pub trait CheckedHeap: Heap {
-  //
+  /// Returns `true` if the given pointer and access length is valid.
+  fn is_valid(&self, ptr: u64, len: usize) -> bool;
 }
 
 /// Heap that uses system's allocator to allocate memory.
 #[derive(Default)]
 pub struct System {
+  mems: HashMap<u64, Mem>,
   size: usize,
-  mems: Vec<Mem>,
+}
+
+impl System {
+  /// Allocates a new memory.
+  fn alloc(&mut self, layout: Layout, obj: Option<Obj>) -> u64 {
+    let mem = unsafe { Mem::new(layout, obj) };
+    let ptr = mem.data.as_ptr() as u64;
+    self.mems.insert(ptr, mem);
+    self.size += layout.size();
+    ptr
+  }
 }
 
 impl Heap for System {
   fn new() -> Self {
     Self::default()
   }
+
+  fn alloc(&mut self, layout: Layout) -> u64 {
+    self.alloc(layout, None)
+  }
+
+  fn alloc_obj(&mut self, layout: Layout, obj: Obj) -> u64 {
+    self.alloc(layout, Some(obj))
+  }
+
+  fn dealloc(&mut self, ptr: u64) {
+    if let Some(mem) = self.mems.remove(&ptr) {
+      self.size -= mem.data.len();
+    }
+  }
+
+  fn size(&self) -> usize {
+    self.size
+  }
+
+  fn addr(&self, ptr: u64) -> *const () {
+    ptr as *const ()
+  }
+
+  fn addr_mut(&mut self, ptr: u64) -> *mut () {
+    ptr as *mut ()
+  }
+
+  fn obj(&self, ptr: u64) -> Option<&Obj> {
+    self.mems.get(&ptr).and_then(|m| m.obj.as_ref())
+  }
 }
 
 /// Allocated system heap memory.
 pub struct Mem {
-  obj: Option<Obj>,
   data: Box<[u8]>,
+  obj: Option<Obj>,
+}
+
+impl Mem {
+  /// Create a new memory from the given layout and object metadata.
+  ///
+  /// # Safety
+  ///
+  /// The allocated memory is uninitialized.
+  unsafe fn new(layout: Layout, obj: Option<Obj>) -> Self {
+    let ptr = alloc::alloc(layout);
+    if ptr.is_null() {
+      alloc::handle_alloc_error(layout);
+    }
+    let data = Box::from_raw(slice::from_raw_parts_mut(ptr, layout.size()));
+    Self { data, obj }
+  }
 }
 
 /// Object metadata for heap memory.
+#[derive(Debug)]
 pub struct Obj {
-  index: usize,
-  is_array: bool,
+  kind: ObjKind,
+  addr: ObjAddr,
+}
+
+impl Obj {
+  /// Creates a new object metadata.
+  pub fn new(kind: ObjKind, addr: ObjAddr) -> Self {
+    Self { kind, addr }
+  }
+}
+
+/// Kind of object.
+#[derive(Debug)]
+pub enum ObjKind {
+  /// Just an object.
+  Obj,
+  /// An array of objects.
+  ///
+  /// With the length of the array.
+  Array(usize),
+}
+
+/// Address of object metadata.
+#[derive(Debug)]
+pub enum ObjAddr {
+  /// Object metadata is on the heap.
+  ///
+  /// With a heap memory pointer.
+  Heap(u64),
+  /// Object metadata is in the constant pool.
+  ///
+  /// With a constant pool index.
+  Const(usize),
 }
 
 /// Heap with memory out of bounds checking.
 pub struct Checked<H> {
   heap: H,
+  ranges: BTreeMap<usize, usize>,
+}
+
+impl<H> Checked<H> {
+  /// Adds a new range from the given address and size.
+  fn add_range(&mut self, addr: *const (), size: usize) {
+    todo!()
+  }
 }
 
 impl<H> Heap for Checked<H>
@@ -44,7 +170,42 @@ where
   H: Heap,
 {
   fn new() -> Self {
-    Self { heap: H::new() }
+    Self {
+      heap: H::new(),
+      ranges: BTreeMap::new(),
+    }
+  }
+
+  fn alloc(&mut self, layout: Layout) -> u64 {
+    let ptr = self.heap.alloc(layout);
+    self.add_range(self.addr(ptr), layout.size());
+    ptr
+  }
+
+  fn alloc_obj(&mut self, layout: Layout, obj: Obj) -> u64 {
+    let ptr = self.heap.alloc_obj(layout, obj);
+    self.add_range(self.addr(ptr), layout.size());
+    ptr
+  }
+
+  fn dealloc(&mut self, ptr: u64) {
+    todo!()
+  }
+
+  fn size(&self) -> usize {
+    self.heap.size()
+  }
+
+  fn addr(&self, ptr: u64) -> *const () {
+    self.heap.addr(ptr)
+  }
+
+  fn addr_mut(&mut self, ptr: u64) -> *mut () {
+    self.heap.addr_mut(ptr)
+  }
+
+  fn obj(&self, ptr: u64) -> Option<&Obj> {
+    self.heap.obj(ptr)
   }
 }
 
@@ -52,5 +213,8 @@ impl<H> CheckedHeap for Checked<H>
 where
   H: Heap,
 {
-  // TODO
+  fn is_valid(&self, ptr: u64, len: usize) -> bool {
+    let addr = self.heap.addr(ptr);
+    todo!()
+  }
 }
