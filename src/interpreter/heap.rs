a2129d9426c072129dbe7de68916f757077ba418
MaxXSoft <x@MaxXSoft.net>
2023-08-06 14:19:06 +0800

    (heap) added trait `CheckHeap` and more implementations

--- src/interpreter/heap.rs
+++ src/interpreter/heap.rs
@@ -3,21 +3,30 @@ pub trait Heap {
   //
 }
 
-/// Heap that do not check for memory out of bounds.
+/// Managed heap with memory out of bounds checking.
+pub trait CheckedHeap: Heap {
+  //
+}
+
+/// Heap that uses system's allocator to allocate memory.
 #[derive(Default)]
-pub struct UncheckedHeap {
+pub struct System {
   size: usize,
   mems: Vec<Mem>,
 }
 
-impl UncheckedHeap {
+impl System {
   /// Creates a new heap.
   pub fn new() -> Self {
     Self::default()
   }
 }
 
-/// Allocated heap memory.
+impl Heap for System {
+  // TODO
+}
+
+/// Allocated system heap memory.
 pub struct Mem {
   obj: Option<Obj>,
   data: Box<[u8]>,
@@ -27,4 +36,23 @@ pub struct Mem {
 pub struct Obj {
   index: usize,
   is_array: bool,
+}
+
+/// Heap with memory out of bounds checking.
+pub struct Checked<H> {
+  heap: H,
+}
+
+impl<H> Heap for Checked<H>
+where
+  H: Heap,
+{
+  // TODO
+}
+
+impl<H> CheckedHeap for Checked<H>
+where
+  H: Heap,
+{
+  // TODO
 }
