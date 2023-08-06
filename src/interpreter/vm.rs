c983b9bcaf9e5e52daa49bf01092bf066898f15c
MaxXSoft <x@MaxXSoft.net>
2023-08-06 13:35:37 +0800

    (vm) updated implementations

--- src/interpreter/vm.rs
+++ src/interpreter/vm.rs
@@ -1,38 +1,54 @@
 use crate::bytecode::consts::Const;
 use crate::bytecode::insts::Inst;
+use crate::bytecode::reader::Reader;
+use crate::interpreter::policy::Policy;
 
 /// Virtual machine for running bytecode.
-pub struct VM {
+pub struct VM<P: Policy> {
+  policy: P,
   consts: Box<[Const]>,
   insts: Box<[Inst]>,
   pc: u64,
-  value_stack: Vec<Value>,
-  local_stack: Vec<Vars>,
-  globals: Vars,
+  value_stack: Vec<P::Value>,
+  local_stack: Vec<Vars<P::Value>>,
+  globals: Vars<P::Value>,
+  heap: P::Heap,
+  gc: P::GarbageCollector,
 }
 
-impl VM {
+impl<P: Policy> VM<P> {
   /// Creates a new virtual machine from the given constants and instructions.
-  pub fn new(consts: Box<[Const]>, insts: Box<[Inst]>) -> Self {
+  pub fn new(policy: P, consts: Box<[Const]>, insts: Box<[Inst]>) -> Self {
+    let heap = policy.new_heap();
+    let gc = policy.new_gc();
     Self {
+      policy,
       consts,
       insts,
       pc: 0,
       value_stack: vec![],
       local_stack: vec![],
-      globals: Default::default(),
+      globals: Vars::new(),
+      heap,
+      gc,
     }
+  }
+
+  /// Creates a new virtual machine from the given [`Reader`].
+  pub fn from_reader<R>(policy: P, reader: Reader<R>) -> Self {
+    let (consts, insts) = reader.into_consts_insts();
+    Self::new(policy, consts, insts)
   }
 }
 
-/// Value.
-struct Value {
-  is_ptr: bool,
-  value: u64,
+/// Variable storage.
+struct Vars<V> {
+  vars: Vec<V>,
 }
 
-/// Variable storage.
-#[derive(Default)]
-struct Vars {
-  vars: Vec<Value>,
+impl<V> Vars<V> {
+  /// Creates a new variable storage.
+  fn new() -> Self {
+    Self { vars: vec![] }
+  }
 }
