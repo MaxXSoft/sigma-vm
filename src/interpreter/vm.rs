65bcece04c80f3013fdb3a020b5f445fe40b6b02
MaxXSoft <x@MaxXSoft.net>
2023-08-08 15:21:11 +0800

    (vm) added helper trait `IntoU64`

--- src/interpreter/vm.rs
+++ src/interpreter/vm.rs
@@ -227,3 +227,27 @@ enum InstAction {
   SetPC(u64),
   Stop,
 }
+
+/// Converts the given value into a 64-bit unsigned integer.
+trait IntoU64 {
+  fn into_u64(self) -> u64;
+}
+
+/// Helper macros for implementing [`IntoU64`] for the given type.
+macro_rules! impl_into_u64 {
+  ($ty:ty) => {
+    impl IntoU64 for $ty {
+      fn into_u64(self) -> u64 {
+        self as u64
+      }
+    }
+  };
+}
+
+impl_into_u64!(i8);
+impl_into_u64!(u8);
+impl_into_u64!(i16);
+impl_into_u64!(u16);
+impl_into_u64!(i32);
+impl_into_u64!(u32);
+impl_into_u64!(u64);
