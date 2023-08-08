4ed5253888ffcdab4559a24e76a719491f3d6ebc
MaxXSoft <x@MaxXSoft.net>
2023-08-08 21:16:05 +0800

    (bytecode) added constant `VERSION`

--- src/bytecode/mod.rs
+++ src/bytecode/mod.rs
@@ -5,3 +5,10 @@ pub mod writer;
 
 /// Magic number of the bytecode file.
 const MAGIC: [u8; 4] = [b'S', b'g', b'm', b'a'];
+
+/// Current version of the bytecode file.
+const VERSION: [u64; 3] = [
+  crate::utils::str_to_u64(env!("CARGO_PKG_VERSION_MAJOR")),
+  crate::utils::str_to_u64(env!("CARGO_PKG_VERSION_MINOR")),
+  crate::utils::str_to_u64(env!("CARGO_PKG_VERSION_PATCH")),
+];
