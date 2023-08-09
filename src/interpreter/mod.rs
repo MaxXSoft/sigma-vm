04661fab1855be6932f410b90702879017799ed0
MaxXSoft <x@MaxXSoft.net>
2023-08-09 15:21:32 +0800

    (interpreter) added unit test `doubly_linked_list`

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -143,4 +143,133 @@ mod test {
     assert_eq!(fib(10), 55);
     assert_eq!(fib(20), 6765);
   }
+
+  #[test]
+  fn doubly_linked_list() {
+    fn sum_repeat(n: u64, repeat: u64) -> u64 {
+      vm! {
+        insts: [
+          StA(1),
+          Call(2), // main
+          Ret,
+
+        // main:
+          PushU(0),
+          StV(0),
+          LdG(1),
+        // loop_start:
+          Dup,
+          Bz(10), // loop_end
+          // create a new list
+          LdG(0),
+          Call(11), // new_list
+          // get sum
+          Call(59), // sum
+          LdV(0),
+          Add,
+          StV(0),
+          PushU(1),
+          Sub,
+          Jmp(-10), // loop_start
+        // loop_end:
+          Pop,
+          LdV(0),
+          Ret,
+
+        // new_list:
+          StA(0),
+          PushU(0),
+          Dup,
+          StV(2),
+          StV(1),
+          LdV(0),
+        // loop_start:
+          Dup,
+          Bz(19), // loop_end
+          // create a new node
+          Dup,
+          LdV(0),
+          Swap,
+          Sub,
+          Call(17), // new_node
+          // check tail
+          LdV(2),
+          Bz(6), // else
+          Dup,
+          LdV(2),
+          Swap,
+          Call(23), // append
+          Jmp(3), // end if
+        // else:
+          Dup,
+          StV(1),
+        // end_if:
+          StV(2),
+          PushU(1),
+          Sub,
+          Jmp(-19), // loop_start
+        // loop_end:
+          Pop,
+          LdV(1),
+          Ret,
+
+        // new_node:
+          StA(0),
+          NewOC(0),
+          Dup,
+          PushU(0),
+          StDO(0),
+          Dup,
+          PushU(0),
+          StDO(1),
+          Dup,
+          LdV(0),
+          StDO(2),
+          Ret,
+        
+        // append:
+          StA(1),
+          LdV(0),
+          LdV(1),
+          StDO(1),
+          LdV(1),
+          LdV(0),
+          StDO(0),
+          Ret,
+        
+        // sum:
+          StA(0),
+          PushU(0),
+          StV(1),
+          LdV(0),
+        // loop_start:
+          Dup,
+          Bz(8), // loop_end
+          Dup,
+          LdDO(2),
+          LdV(1),
+          Add,
+          StV(1),
+          LdPO(1),
+          Jmp(-8), // loop_start
+        // loop_end:
+          Pop,
+          LdV(1),
+          Ret,
+        ],
+        consts: [
+          Object {
+            size: 24,
+            align: 8,
+            managed_ptr: ManagedPtr { len: 2, offsets: [0, 1] }
+          },
+        ],
+        args: [u64: n, u64: repeat],
+        results: (u64),
+      }
+    }
+
+    assert_eq!(sum_repeat(5, 1000), 10000);
+    assert_eq!(sum_repeat(10, 10000), 450000);
+  }
 }
