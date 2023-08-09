08bd96dad822c4a83210455d4d49d3602c8e75fb
MaxXSoft <x@MaxXSoft.net>
2023-08-09 19:49:55 +0800

    (main) added struct `Size`

--- sigma/src/main.rs
+++ sigma/src/main.rs
@@ -1,4 +1,6 @@
 use clap::{Parser, ValueEnum};
+use std::fmt;
+use std::str::FromStr;
 
 /// Sigma Virtual Machine.
 #[derive(Parser, Debug)]
@@ -20,8 +22,8 @@ struct CommandLineArgs {
   gc: GarbageCollector,
 
   /// Garbage collector threshold.
-  #[arg(short = 't', long, default_value_t = 1024 * 1024 * 1024)]
-  gc_threshold: usize,
+  #[arg(short = 't', long, default_value_t = Size::Megabytes(256))]
+  gc_threshold: Size,
 }
 
 #[derive(ValueEnum, Clone, Copy, Debug)]
@@ -52,6 +54,61 @@ enum GarbageCollector {
   /// Mark-sweep garbage collector.
   #[value(alias("ms"))]
   MarkSweep,
+}
+
+/// Size in different units: B, KB, MB, GB.
+#[derive(Clone, Copy, Debug)]
+enum Size {
+  Bytes(usize),
+  Kilobytes(usize),
+  Megabytes(usize),
+  Gigabytes(usize),
+}
+
+impl From<Size> for usize {
+  fn from(size: Size) -> Self {
+    match size {
+      Size::Bytes(b) => b,
+      Size::Kilobytes(kb) => kb * 1024,
+      Size::Megabytes(mb) => mb * 1024 * 1024,
+      Size::Gigabytes(gb) => gb * 1024 * 1024 * 1024,
+    }
+  }
+}
+
+impl FromStr for Size {
+  type Err = &'static str;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let s = s.to_ascii_lowercase();
+    let digits = match s.find(|c: char| !c.is_ascii_digit()) {
+      Some(i) => &s.as_str()[..i],
+      None => &s,
+    };
+    let size: usize = digits.parse().unwrap();
+    if s.len() == digits.len() || s.ends_with("b") {
+      Ok(Self::Bytes(size))
+    } else if s.ends_with("k") || s.ends_with("kb") {
+      Ok(Self::Kilobytes(size))
+    } else if s.ends_with("m") || s.ends_with("mb") {
+      Ok(Self::Megabytes(size))
+    } else if s.ends_with("g") || s.ends_with("gb") {
+      Ok(Self::Gigabytes(size))
+    } else {
+      Err("size can only end with B/KB/MB/GB")
+    }
+  }
+}
+
+impl fmt::Display for Size {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      Self::Bytes(b) => write!(f, "{b}B"),
+      Self::Kilobytes(kb) => write!(f, "{kb}KB"),
+      Self::Megabytes(mb) => write!(f, "{mb}MB"),
+      Self::Gigabytes(gb) => write!(f, "{gb}GB"),
+    }
+  }
 }
 
 fn main() {
