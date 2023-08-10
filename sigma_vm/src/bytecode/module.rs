a21c246e87678978a98ad2e36461e3767abf4260
MaxXSoft <x@MaxXSoft.net>
2023-08-10 16:38:57 +0800

    (module) added more methods

--- sigma_vm/src/bytecode/module.rs
+++ sigma_vm/src/bytecode/module.rs
@@ -1,5 +1,21 @@
+use crate::bytecode::consts::HeapConst;
+use crate::bytecode::insts::Inst;
+
 /// Module, contains all static data in a bytecode file,
-/// including instructions, constants, etc.
+/// including constants, instructions, etc.
 pub struct Module {
-  //
+  pub(in crate::bytecode) consts: Box<[HeapConst]>,
+  pub(in crate::bytecode) insts: Box<[Inst]>,
+}
+
+impl Module {
+  /// Returns a reference to the constant pool.
+  pub fn consts(&self) -> &[HeapConst] {
+    &self.consts
+  }
+
+  /// Returns a reference to the instruction list.
+  pub fn insts(&self) -> &[Inst] {
+    &self.insts
+  }
 }
