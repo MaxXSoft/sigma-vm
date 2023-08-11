2c3e9ea8d2fdcfaa04a2a04617894789c926c4a9
MaxXSoft <x@MaxXSoft.net>
2023-08-11 14:45:50 +0800

    (export) updated definitions of export information

--- sigma_vm/src/bytecode/export.rs
+++ sigma_vm/src/bytecode/export.rs
@@ -1,24 +1,55 @@
 use crate::bytecode::consts::{Array, Str};
 use std::collections::HashMap;
+use std::num::NonZeroU64;
+
+/// Exported call site information.
+pub type ExportInfo = HashMap<String, CallSite>;
 
 /// Export information.
-pub type ExportInfo = HashMap<String, PcRets>;
-
-/// Export information entry.
 ///
-/// With function's program counter, number of returned values,
-/// and a string ([`Str`]) of function name.
+/// With call site information and a string ([`Str`]) of function name.
 #[repr(C)]
 #[derive(Debug)]
 pub struct Export<Bytes: ?Sized + Array<u8>> {
-  pub pc_rets: PcRets,
+  pub call_site: CallSite,
   pub name: Str<Bytes>,
 }
 
-/// Pair of function's program counter and number of returned values.
+/// Exported call site.
+///
+/// With function's program counter, number of arguments
+/// and number of returned values.
 #[repr(C)]
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct PcRets {
+pub struct CallSite {
   pub pc: u64,
+  pub num_args: NumArgs,
   pub num_rets: u64,
+}
+
+/// Number of arguments.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum NumArgs {
+  /// Variadic, the actual argument number
+  /// will be placed on the top of the stack.
+  Variadic,
+  /// Number of arguments plus one.
+  PlusOne(NonZeroU64),
+}
+
+impl From<u64> for NumArgs {
+  fn from(n: u64) -> Self {
+    NonZeroU64::new(n)
+      .map(Self::PlusOne)
+      .unwrap_or(Self::Variadic)
+  }
+}
+
+impl From<NumArgs> for u64 {
+  fn from(num_args: NumArgs) -> Self {
+    match num_args {
+      NumArgs::Variadic => 0,
+      NumArgs::PlusOne(n) => n.get(),
+    }
+  }
 }
