0be4e0644ab66de080838a76e2d4ef6ff4da8a9f
MaxXSoft <x@MaxXSoft.net>
2023-08-11 13:07:20 +0800

    (module) added field `exports` for `Module`

--- sigma_vm/src/bytecode/module.rs
+++ sigma_vm/src/bytecode/module.rs
@@ -1,4 +1,5 @@
 use crate::bytecode::consts::HeapConst;
+use crate::bytecode::export::{ExportInfo, PcRets};
 use crate::bytecode::insts::Inst;
 
 /// Module, contains all static data in a bytecode file,
@@ -6,6 +7,7 @@ use crate::bytecode::insts::Inst;
 #[derive(Debug)]
 pub struct Module {
   pub(crate) consts: Box<[HeapConst]>,
+  pub(crate) exports: ExportInfo,
   pub(crate) insts: Box<[Inst]>,
 }
 
@@ -13,6 +15,11 @@ impl Module {
   /// Returns a reference to the constant pool.
   pub fn consts(&self) -> &[HeapConst] {
     &self.consts
+  }
+
+  /// Returns a reference to the export information of the given function.
+  pub fn export(&self, name: &str) -> Option<&PcRets> {
+    self.exports.get(name)
   }
 
   /// Returns a reference to the instruction list.
