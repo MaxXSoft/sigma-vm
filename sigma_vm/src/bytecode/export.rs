fcaffb177afcd9909401f97d4fad06c97f8abcd1
MaxXSoft <x@MaxXSoft.net>
2023-08-11 12:53:42 +0800

    (export) added struct `PcRets`

--- sigma_vm/src/bytecode/export.rs
+++ sigma_vm/src/bytecode/export.rs
@@ -7,7 +7,14 @@ use crate::bytecode::consts::{Array, Str};
 #[repr(C)]
 #[derive(Debug)]
 pub struct Export<Bytes: ?Sized + Array<u8>> {
+  pub pc_rets: PcRets,
+  pub name: Str<Bytes>,
+}
+
+/// Pair of function's program counter and number of returned values.
+#[repr(C)]
+#[derive(Debug)]
+pub struct PcRets {
   pub pc: u64,
   pub num_rets: u64,
-  pub name: Str<Bytes>,
 }
