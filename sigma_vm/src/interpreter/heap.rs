2f553cee2f48fc8483c199a8dbe7ff6e7b32cd83
MaxXSoft <x@MaxXSoft.net>
2023-08-16 17:27:15 +0800

    (heap) supported heap memory metadata

--- sigma_vm/src/interpreter/heap.rs
+++ sigma_vm/src/interpreter/heap.rs
@@ -1,4 +1,3 @@
-use crate::interpreter::loader::Source;
 use std::alloc::{self, Layout};
 use std::collections::{BTreeMap, HashMap};
 use std::{fmt, ops, slice};
@@ -110,13 +109,9 @@ pub trait Heap {
   /// Creates a new heap.
   fn new() -> Self;
 
-  /// Allocates a new memory with the given layout.
+  /// Allocates a new memory with the given layout and metadata.
   /// Returns the pointer of the allocated memory.
-  fn alloc(&mut self, layout: Layout) -> Ptr;
-
-  /// Allocates a new memory with the given layout and object metadata.
-  /// Returns the pointer of the allocated memory.
-  fn alloc_obj(&mut self, layout: Layout, obj: Obj) -> Ptr;
+  fn alloc(&mut self, layout: Layout, meta: Meta) -> Ptr;
 
   /// Deallocates the given pointer.
   fn dealloc(&mut self, ptr: Ptr);
@@ -134,9 +129,9 @@ pub trait Heap {
   /// Returns the mutable memory address of the given pointer.
   fn addr_mut(&mut self, ptr: Ptr) -> *mut ();
 
-  /// Returns a reference to the heap object metadata of the given pointer, or
-  /// [`None`] if the corresponding heap memory is not allocated for an object.
-  fn obj(&self, ptr: Ptr) -> Option<&Obj>;
+  /// Returns a reference to the metadata of the given heap pointer, or
+  /// [`None`] if the given pointer is invalid.
+  fn meta(&self, ptr: Ptr) -> Option<&Meta>;
 
   /// Returns a vector of pointers of all allocated memory.
   fn ptrs(&self) -> Vec<Ptr>;
@@ -145,14 +140,27 @@ pub trait Heap {
   fn reset(&mut self);
 }
 
+/// Metadata for heap memory.
+#[derive(Debug, Clone, Copy)]
+pub enum Meta {
+  /// Memory stores raw data.
+  Raw,
+  /// Memory stores an object.
+  Obj(Obj),
+  /// Memory stores a module handle.
+  Module,
+  /// Memory stores a native shared library handle.
+  Native,
+}
+
 /// Object metadata for heap memory.
 #[derive(Debug, Clone, Copy)]
 pub struct Obj {
   pub kind: ObjKind,
   /// Pointer to the real object metadata.
   pub ptr: Ptr,
-  /// Source of the module where the object was allocated.
-  pub source: Source,
+  /// Handle of the module where the object was allocated.
+  pub module: Ptr,
 }
 
 /// Kind of object.
@@ -179,28 +187,17 @@ pub struct System {
   size: usize,
 }
 
-impl System {
-  /// Allocates a new memory.
-  fn alloc(&mut self, layout: Layout, obj: Option<Obj>) -> Ptr {
-    let mem = unsafe { Mem::new(layout, obj) };
-    let ptr = (mem.data.as_ptr() as u64).into();
-    self.mems.insert(ptr, mem);
-    self.size += layout.size();
-    ptr
-  }
-}
-
 impl Heap for System {
   fn new() -> Self {
     Self::default()
   }
 
-  fn alloc(&mut self, layout: Layout) -> Ptr {
-    self.alloc(layout, None)
-  }
-
-  fn alloc_obj(&mut self, layout: Layout, obj: Obj) -> Ptr {
-    self.alloc(layout, Some(obj))
+  fn alloc(&mut self, layout: Layout, meta: Meta) -> Ptr {
+    let mem = unsafe { Mem::new(layout, meta) };
+    let ptr = (mem.data.as_ptr() as u64).into();
+    self.mems.insert(ptr, mem);
+    self.size += layout.size();
+    ptr
   }
 
   fn dealloc(&mut self, ptr: Ptr) {
@@ -225,8 +222,8 @@ impl Heap for System {
     u64::from(ptr) as *mut ()
   }
 
-  fn obj(&self, ptr: Ptr) -> Option<&Obj> {
-    self.mems.get(&ptr).and_then(|m| m.obj.as_ref())
+  fn meta(&self, ptr: Ptr) -> Option<&Meta> {
+    self.mems.get(&ptr).map(|m| &m.meta)
   }
 
   fn ptrs(&self) -> Vec<Ptr> {
@@ -242,22 +239,23 @@ impl Heap for System {
 /// Allocated system heap memory.
 pub struct Mem {
   data: Box<[u8]>,
-  obj: Option<Obj>,
+  meta: Meta,
 }
 
 impl Mem {
-  /// Create a new memory from the given layout and object metadata.
+  /// Create a new memory from the given layout and metadata.
   ///
   /// # Safety
   ///
-  /// The allocated memory is uninitialized.
-  unsafe fn new(layout: Layout, obj: Option<Obj>) -> Self {
+  /// The allocated memory is uninitialized, so it must be written
+  /// before reading.
+  unsafe fn new(layout: Layout, meta: Meta) -> Self {
     let ptr = alloc::alloc(layout);
     if ptr.is_null() {
       alloc::handle_alloc_error(layout);
     }
     let data = Box::from_raw(slice::from_raw_parts_mut(ptr, layout.size()));
-    Self { data, obj }
+    Self { data, meta }
   }
 }
 
@@ -297,14 +295,8 @@ where
     }
   }
 
-  fn alloc(&mut self, layout: Layout) -> Ptr {
-    let ptr = self.heap.alloc(layout);
-    self.add_range(self.addr(ptr), layout.size());
-    ptr
-  }
-
-  fn alloc_obj(&mut self, layout: Layout, obj: Obj) -> Ptr {
-    let ptr = self.heap.alloc_obj(layout, obj);
+  fn alloc(&mut self, layout: Layout, meta: Meta) -> Ptr {
+    let ptr = self.heap.alloc(layout, meta);
     self.add_range(self.addr(ptr), layout.size());
     ptr
   }
@@ -347,8 +339,8 @@ where
     self.heap.addr_mut(ptr)
   }
 
-  fn obj(&self, ptr: Ptr) -> Option<&Obj> {
-    self.heap.obj(ptr)
+  fn meta(&self, ptr: Ptr) -> Option<&Meta> {
+    self.heap.meta(ptr)
   }
 
   fn ptrs(&self) -> Vec<Ptr> {
