f7105b7bf1a28a2d9c24bfc2f81f16e534b02e0e
MaxXSoft <x@MaxXSoft.net>
2023-08-17 11:08:35 +0800

    (module) added struct `StaticModule`

--- sigma_vm/src/bytecode/module.rs
+++ sigma_vm/src/bytecode/module.rs
@@ -1,9 +1,12 @@
-use crate::bytecode::consts::HeapConst;
+use crate::bytecode::consts::{Const, HeapConst};
 use crate::bytecode::export::{CallSite, ExportInfo};
 use crate::bytecode::insts::Inst;
+use crate::interpreter::heap::Heap;
 
 /// Module, contains all static data in a bytecode file,
 /// including constants, instructions, etc.
+///
+/// [`Module`] can be executed directly by the virtual machine.
 #[derive(Debug)]
 pub struct Module {
   pub(crate) consts: Box<[HeapConst]>,
@@ -25,5 +28,32 @@ impl Module {
   /// Returns a reference to the instruction list.
   pub fn insts(&self) -> &[Inst] {
     &self.insts
+  }
+}
+
+/// Static module, which can not be executed directly by the virtual machine
+/// without converting into a [`Module`].
+pub struct StaticModule {
+  pub consts: Box<[Const]>,
+  pub exports: ExportInfo,
+  pub insts: Box<[Inst]>,
+}
+
+impl StaticModule {
+  /// Converts the current static module into a [`Module`].
+  pub fn into_module<H>(self, heap: &mut H) -> Module
+  where
+    H: Heap,
+  {
+    Module {
+      consts: self
+        .consts
+        .into_vec()
+        .into_iter()
+        .map(|c| c.into_heap_const(heap))
+        .collect(),
+      exports: self.exports,
+      insts: self.insts,
+    }
   }
 }
