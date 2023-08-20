8a582993cec7103fd860054fcb80bce8b04231d7
MaxXSoft <x@MaxXSoft.net>
2023-08-20 13:59:49 +0800

    (anf) added more implementations

--- lisp/src/anf.rs
+++ lisp/src/anf.rs
@@ -1,7 +1,24 @@
 use crate::front::{ElemKind, Element};
-use laps::return_error;
 use laps::span::{Result, Span};
+use laps::{log_warning, return_error};
 use std::collections::HashMap;
+
+/// Statement.
+#[derive(Debug)]
+pub enum Statement {
+  /// Definition.
+  Define(Define),
+  /// Expression.
+  Expr(Expr),
+}
+
+/// Definition.
+#[derive(Debug)]
+pub struct Define {
+  pub name: String,
+  pub var: u64,
+  pub expr: Expr,
+}
 
 /// Expression.
 #[derive(Debug)]
@@ -29,6 +46,8 @@ pub enum Value {
   Var(u64),
   /// Variable reference from an outer scope.
   OuterVar(u64),
+  /// Variable reference from the global scope.
+  GlobalVar(u64),
   /// Lambda.
   Lambda(Lambda),
 }
@@ -84,26 +103,127 @@ impl Generator {
   }
 
   /// Generates on the given element.
-  pub fn generate(&mut self, elem: Element) -> Expr {
+  pub fn generate(&mut self, elem: Element) -> Result<Option<Statement>> {
+    // handle by kind
     match elem.kind {
-      _ => todo!(),
+      ElemKind::List(es) => {
+        // get the first element
+        let first = es
+          .first()
+          .ok_or_else(|| return_error!(elem.span, "list can not be empty"))?;
+        // handle by kind
+        match &first.kind {
+          ElemKind::Num(_) | ElemKind::Str(_) => {
+            return_error!(first.span, "element is not callable")
+          }
+          ElemKind::Sym(s) => match s.as_str() {
+            "define" => {
+              return self
+                .gen_define(&es[1..], elem.span)
+                .map(|d| Some(Statement::Define(d)))
+            }
+            "lambda" => {}
+            "cond" => {
+              return self
+                .gen_cond(&es[1..], elem.span)
+                .map(|a| Some(Statement::Expr(Expr::CompExpr(CompExpr::If(a)))))
+            }
+            _ => {}
+          },
+          _ => {}
+        };
+        // generate apply
+        return self
+          .gen_apply(&es)
+          .map(|a| Some(Statement::Expr(Expr::CompExpr(CompExpr::Apply(a)))));
+      }
+      _ => {}
+    }
+    // unhandled, just ignore
+    log_warning!(elem.span, "ignored this expression");
+    Ok(None)
+  }
+
+  /// Returns the symbol from the current element.
+  fn sym(elem: &Element) -> Result<String> {
+    match &elem.kind {
+      ElemKind::Sym(s) => Ok(s.clone()),
+      _ => return_error!(elem.span, "expected symbol"),
     }
   }
 
-  /// Generates a value.
-  fn gen_value(&mut self, elem: Element) -> Result<Value> {
-    match elem.kind {
-      ElemKind::Num(n) => Ok(Value::Num(n)),
-      ElemKind::Str(s) => Ok(Value::Str(s)),
-      ElemKind::Sym(s) => self.gen_sym(s, elem.span),
-      ElemKind::Quote(e) => Ok(self.gen_quote(*e)),
+  /// Generates a definition.
+  fn gen_define(&mut self, elems: &[Element], span: Span) -> Result<Define> {
+    if elems.len() != 2 {
+      return_error!(span, "expected exactly 2 arguments")
+    }
+    let name = Self::sym(&elems[0])?;
+    let var = self.env.define(name.clone());
+    let expr = self.gen_expr(&elems[1])?;
+    Ok(Define { name, var, expr })
+  }
+
+  /// Generates a lambda function.
+  fn gen_lambda(&mut self, elems: &[Element], span: Span) -> Result<Lambda> {
+    if elems.len() != 2 {
+      return_error!(span, "expected exactly 2 arguments")
+    }
+    // get parameter list
+    let params = match &elems[0].kind {
+      ElemKind::List(es) => es,
+      _ => return_error!(elems[0].span, "expected parameter list"),
+    };
+    self.env.enter();
+    let params = params
+      .iter()
+      .enumerate()
+      .map(|(i, p)| Self::sym(p).map(|s| self.env.define_arg(s, i as u64)))
+      .collect::<Result<_>>()?;
+    // generate body
+    let expr = Box::new(self.gen_expr(&elems[1])?);
+    self.env.exit();
+    Ok(Lambda { params, expr })
+  }
+
+  /// Generates a conditional expression.
+  fn gen_cond(&mut self, elems: &[Element], span: Span) -> Result<If> {
+    if elems.is_empty() {
+      return_error!(span, "expected at least 1 argument")
+    }
+    // check all conditions
+    for elem in elems {
+      // get condition and expression
+      let (cond, expr) = match &elem.kind {
+        ElemKind::List(es) => match &es[..] {
+          [cond, expr] => (cond, expr),
+          _ => return_error!(elem.span, "expected exactly 2 elements"),
+        },
+        _ => return_error!(elem.span, "expected list"),
+      };
+      //
+    }
+    todo!()
+  }
+
+  /// Generates a function application.
+  fn gen_apply(&mut self, elems: &[Element]) -> Result<Apply> {
+    todo!()
+  }
+
+  /// Generates an expression.
+  fn gen_expr(&mut self, elem: &Element) -> Result<Expr> {
+    match &elem.kind {
+      ElemKind::Num(n) => Ok(Expr::Value(Value::Num(*n))),
+      ElemKind::Str(s) => Ok(Expr::Value(Value::Str(s.clone()))),
+      ElemKind::Sym(s) => self.gen_sym(s, &elem.span).map(Expr::Value),
+      ElemKind::Quote(e) => Ok(Expr::Value(self.gen_quote(&e))),
       ElemKind::List(_) => return_error!(elem.span, "invalid value"),
     }
   }
 
   /// Generates a symbol.
-  fn gen_sym(&mut self, sym: String, span: Span) -> Result<Value> {
-    match self.env.get(&sym) {
+  fn gen_sym(&mut self, sym: &str, span: &Span) -> Result<Value> {
+    match self.env.get(sym) {
       Some(VarKind::Var(id)) => Ok(Value::Var(id)),
       Some(VarKind::OuterVar(id)) => Ok(Value::OuterVar(id)),
       None => return_error!(span, "symbol {sym} not found"),
@@ -111,14 +231,19 @@ impl Generator {
   }
 
   /// Generates a quotation.
-  fn gen_quote(&mut self, elem: Element) -> Value {
-    match elem.kind {
-      ElemKind::Num(n) => Value::Num(n),
-      ElemKind::Str(s) => Value::Str(s),
-      ElemKind::Sym(s) => Value::Sym(s),
-      ElemKind::Quote(e) => Value::List(vec![Value::Sym("quote".into()), self.gen_quote(*e)]),
-      ElemKind::List(es) => Value::List(es.into_iter().map(|e| self.gen_quote(e)).collect()),
+  fn gen_quote(&mut self, elem: &Element) -> Value {
+    match &elem.kind {
+      ElemKind::Num(n) => Value::Num(*n),
+      ElemKind::Str(s) => Value::Str(s.clone()),
+      ElemKind::Sym(s) => Value::Sym(s.clone()),
+      ElemKind::Quote(e) => Value::List(vec![Value::Sym("quote".into()), self.gen_quote(&e)]),
+      ElemKind::List(es) => Value::List(es.iter().map(|e| self.gen_quote(e)).collect()),
     }
+  }
+
+  /// Generates on the given list.
+  fn gen_list(&mut self, elems: Vec<Element>, span: Span) -> Result<Expr> {
+    todo!()
   }
 }
 
@@ -148,6 +273,11 @@ impl Env {
   /// Defines a new variable. Returns the variable number.
   fn define(&mut self, name: String) -> u64 {
     self.scopes.last_mut().unwrap().define(name)
+  }
+
+  /// Defines a new argument. Returns the variable number.
+  fn define_arg(&mut self, name: String, i: u64) -> u64 {
+    self.scopes.last_mut().unwrap().define_arg(name, i)
   }
 
   /// Finds the given variable in all scopes.
@@ -183,6 +313,11 @@ impl Scope {
   fn define(&mut self, name: String) -> u64 {
     let id = self.vars.len() as u64;
     *self.vars.entry(name).or_insert(id)
+  }
+
+  /// Defines a new argument. Returns the variable number.
+  fn define_arg(&mut self, name: String, i: u64) -> u64 {
+    *self.vars.entry(name).or_insert(i)
   }
 
   /// Finds the given variable.
