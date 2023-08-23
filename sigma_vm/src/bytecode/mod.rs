a6546336af54355671fa6fcbc860a42a4a60a2f8
MaxXSoft <x@MaxXSoft.net>
2023-08-23 12:54:43 +0800

    (bytecode) simplified some implementations

--- sigma_vm/src/bytecode/mod.rs
+++ sigma_vm/src/bytecode/mod.rs
@@ -7,7 +7,7 @@ pub mod reader;
 pub mod writer;
 
 /// Magic number of the bytecode file.
-const MAGIC: [u8; 4] = [b'S', b'g', b'm', b'a'];
+const MAGIC: [u8; 4] = *b"Sgma";
 
 /// Current version of the bytecode file.
 const VERSION: [u64; 3] = [
