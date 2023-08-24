9f32aeab3174377cad4b495d6244beeeb13b1008
MaxXSoft <x@MaxXSoft.net>
2023-08-24 13:17:56 +0800

    (sbas/main) added some implementations

--- sbas/src/main.rs
+++ sbas/src/main.rs
@@ -1,6 +1,54 @@
 mod assembler;
 mod front;
 
+use clap::Parser;
+use laps::reader::Reader;
+use std::{io, process};
+
+/// Sigma VM bytecode assembler.
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct CommandLineArgs {
+  /// Path to the bytecode assembly, default to standard input.
+  assembly: Option<String>,
+
+  /// Path to the output bytecode file, default to standard output.
+  #[arg(short, long)]
+  output: Option<String>,
+}
+
 fn main() {
-  println!("Hello, world!");
+  let args = CommandLineArgs::parse();
+  // assemble the input file
+  match &args.assembly {
+    Some(path) => match Reader::from_path(path) {
+      Ok(reader) => assemble(&args, reader),
+      Err(e) => {
+        eprintln!("{e}");
+        process::exit(-1);
+      }
+    },
+    None => assemble(&args, Reader::from_stdin()),
+  }
+}
+
+fn assemble<R>(args: &CommandLineArgs, reader: Reader<R>)
+where
+  R: io::Read,
+{
+  // run parser
+  let mut parser = front::Parser::new(reader);
+  let mut assembler = assembler::Assembler::new();
+  loop {
+    let stmt = match parser.parse() {
+      Ok(Some(stmt)) => stmt,
+      Ok(None) => break,
+      Err(_) => return,
+    };
+    // assemble
+    if assembler.generate(stmt).is_err() {
+      return;
+    }
+  }
+  todo!()
 }
