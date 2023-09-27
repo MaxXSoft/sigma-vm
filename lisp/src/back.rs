237cefd9d842222acabe5a4ec1fab1dc011c385a
MaxXSoft <x@MaxXSoft.net>
2023-09-27 20:59:36 +0800

    (lisp/back) updated trait `Generate`

--- lisp/src/back.rs
+++ lisp/src/back.rs
@@ -1,5 +1,5 @@
-use crate::front::{ElemKind, Element};
-use laps::{log_warning, span::Result};
+use crate::anf::*;
+use laps::span::Result;
 use sigma_vm::bytecode::builder::Builder;
 use sigma_vm::bytecode::consts::ObjectRef;
 use sigma_vm::bytecode::insts::Inst;
@@ -33,29 +33,29 @@ impl Generator {
     }
   }
 
-  /// Generates on the given element.
-  pub fn generate(&mut self, elem: Element) -> Result<()> {
-    elem.generate(self)
+  /// Generates on the given statement.
+  pub fn generate(&mut self, stmt: Statement) -> Result<()> {
+    stmt.generate(self)
   }
 
-  /// Generates a quote.
-  fn quote(&mut self, elem: Element) {
-    match elem.kind {
-      ElemKind::Num(num) => self.num(num),
-      ElemKind::Str(s) => self.str(&s),
-      ElemKind::Sym(s) => self.sym(&s),
-      ElemKind::Quote(e) => {
-        self.list(vec![
-          Element {
-            kind: ElemKind::Str("quote".into()),
-            span: elem.span,
-          },
-          *e,
-        ]);
-      }
-      ElemKind::List(elems) => self.list(elems),
-    }
-  }
+  // /// Generates a quote.
+  // fn quote(&mut self, elem: Element) {
+  //   match elem.kind {
+  //     ElemKind::Num(num) => self.num(num),
+  //     ElemKind::Str(s) => self.str(&s),
+  //     ElemKind::Sym(s) => self.sym(&s),
+  //     ElemKind::Quote(e) => {
+  //       self.list(vec![
+  //         Element {
+  //           kind: ElemKind::Str("quote".into()),
+  //           span: elem.span,
+  //         },
+  //         *e,
+  //       ]);
+  //     }
+  //     ElemKind::List(elems) => self.list(elems),
+  //   }
+  // }
 
   /// Generates a new number value.
   fn num(&mut self, num: f64) {
@@ -87,28 +87,28 @@ impl Generator {
     self.builder.inst(Inst::LaC(s));
   }
 
-  /// Generates a new list.
-  fn list(&mut self, elems: Vec<Element>) {
-    self.atom(self.obj_ptr, AtomKind::List);
-    self.builder.inst(Inst::Dup);
-    self.chain(elems.into_iter());
-    self.builder.inst(Inst::StDO(1));
-  }
+  // /// Generates a new list.
+  // fn list(&mut self, elems: Vec<Element>) {
+  //   self.atom(self.obj_ptr, AtomKind::List);
+  //   self.builder.inst(Inst::Dup);
+  //   self.chain(elems.into_iter());
+  //   self.builder.inst(Inst::StDO(1));
+  // }
 
-  /// Chains the given elements into a linked list.
-  fn chain<I>(&mut self, mut elems: I)
-  where
-    I: Iterator<Item = Element>,
-  {
-    if let Some(elem) = elems.next() {
-      self.quote(elem);
-      self.builder.inst(Inst::Dup);
-      self.chain(elems);
-      self.builder.inst(Inst::StDO(2));
-    } else {
-      self.builder.inst(Inst::PushU(0));
-    }
-  }
+  // /// Chains the given elements into a linked list.
+  // fn chain<I>(&mut self, mut elems: I)
+  // where
+  //   I: Iterator<Item = Element>,
+  // {
+  //   if let Some(elem) = elems.next() {
+  //     self.quote(elem);
+  //     self.builder.inst(Inst::Dup);
+  //     self.chain(elems);
+  //     self.builder.inst(Inst::StDO(2));
+  //   } else {
+  //     self.builder.inst(Inst::PushU(0));
+  //   }
+  // }
 
   /// Generates a new atom, leaves the 2nd field uninitialized.
   fn atom(&mut self, obj: u64, kind: AtomKind) {
@@ -140,13 +140,75 @@ trait Generate {
   fn generate(self, generator: &mut Generator) -> Result<()>;
 }
 
-impl Generate for Element {
+impl Generate for Statement {
   fn generate(self, generator: &mut Generator) -> Result<()> {
-    if let ElemKind::List(es) = self.kind {
-      todo!()
-    } else {
-      log_warning!(self.span, "ignored this element");
-      Ok(())
+    match self {
+      Self::Define(d) => d.generate(generator),
+      Self::Expr(e) => e.generate(generator),
     }
+  }
+}
+
+impl Generate for Define {
+  fn generate(self, generator: &mut Generator) -> Result<()> {
+    todo!()
+  }
+}
+
+impl Generate for Expr {
+  fn generate(self, generator: &mut Generator) -> Result<()> {
+    match self {
+      Self::Value(v) => v.generate(generator),
+      Self::CompExpr(c) => c.generate(generator),
+      Self::Let(l) => l.generate(generator),
+    }
+  }
+}
+
+impl Generate for Value {
+  fn generate(self, generator: &mut Generator) -> Result<()> {
+    match self {
+      Self::Num(n) => todo!(),
+      Self::Str(s) => todo!(),
+      Self::Sym(s) => todo!(),
+      Self::List(l) => todo!(),
+      Self::Var(v) => todo!(),
+      Self::GlobalVar(g) => todo!(),
+      Self::Builtin(b) => todo!(),
+      Self::Lambda(l) => l.generate(generator),
+    }
+  }
+}
+
+impl Generate for CompExpr {
+  fn generate(self, generator: &mut Generator) -> Result<()> {
+    match self {
+      Self::Apply(a) => a.generate(generator),
+      Self::If(i) => i.generate(generator),
+    }
+  }
+}
+
+impl Generate for Let {
+  fn generate(self, generator: &mut Generator) -> Result<()> {
+    todo!()
+  }
+}
+
+impl Generate for Lambda {
+  fn generate(self, generator: &mut Generator) -> Result<()> {
+    todo!()
+  }
+}
+
+impl Generate for Apply {
+  fn generate(self, generator: &mut Generator) -> Result<()> {
+    todo!()
+  }
+}
+
+impl Generate for If {
+  fn generate(self, generator: &mut Generator) -> Result<()> {
+    todo!()
   }
 }
